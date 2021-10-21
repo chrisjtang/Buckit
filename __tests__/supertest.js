9e96d082a8be49220e04c14bad12b653be8aa9d9
chrisjtang <chrisjtang@gmail.com>
2021-10-20 17:23:43 -0700

    added edit and delete buttons to frontend

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -4,7 +4,7 @@ const server = 'http:://localhost:3000';
 
 
 describe('Route integration', () => {
-    describe('/', () => {
+    describe('/home/:username', () => {
         describe('GET', () => {
         //   // Note that we return the evaluation of `request` here! It evaluates to
         //   // a promise, so Jest knows not to say this test passes until that
